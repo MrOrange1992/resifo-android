e2c551c8254d8e7a6425729d5f58676765eaa1f0
ellifellner <fellnerelisabeth@gmx.net>
2017-02-04 17:36:25 +0100

    Finished Layout

--- app/src/main/scala/at/reservoirdogs/resifo_android/Wohnsitz1Activity.scala
+++ app/src/main/scala/at/reservoirdogs/resifo_android/Wohnsitz1Activity.scala
@@ -1,16 +1,18 @@
 package at.reservoirdogs.resifo_android
 
 import android.content.Intent
+import android.location.{Location, LocationListener, LocationManager}
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.RadioButton
+import android.widget.{Button, RadioButton, TextView}
 
 /**
   * Created by felix on 01/02/2017.
   */
 class Wohnsitz1Activity extends AppCompatActivity
 {
+
   override protected def onCreate(savedInstanceState: Bundle)
   {
     super.onCreate(savedInstanceState)
@@ -25,4 +27,21 @@ class Wohnsitz1Activity extends AppCompatActivity
       startActivity(new Intent(this, classOf[Wohnsitz2Activity]))
   }
 
+  /*def getAddress: Unit = {
+
+    val b = findViewById(R.id.btnCurrentLocation).asInstanceOf[Button];
+    val locationManager = getSystemService(LOCATION_SERVICE).asInstanceOf[LocationManager];
+
+    val listener = new LocationListener {
+
+      override def onProviderEnabled(provider: String): Unit = ???
+
+      override def onStatusChanged(provider: String, status: Int, extras: Bundle): Unit = ???
+
+      override def onLocationChanged(location: Location): Unit = ???
+
+      override def onProviderDisabled(provider: String): Unit = ???
+    }
+
+  }*/
 }
