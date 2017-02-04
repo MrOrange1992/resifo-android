00b95e9578015df6dc207ce8476a202fba985671
philgoeschl <philipp.goeschl@edu.fh-joanneum.at>
2017-02-04 15:29:29 +0100

    înput1 birthday datepicker gender radio buttons

--- app/src/main/scala/at/reservoirdogs/resifo_android/AnmeldeActivity.scala
+++ app/src/main/scala/at/reservoirdogs/resifo_android/AnmeldeActivity.scala
@@ -4,11 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Toast
 
 class AnmeldeActivity extends AppCompatActivity {
   override protected def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_anmelde)
   }
-  def gotoAnmeldeActivity(view: View): Unit = startActivity(new Intent(this, classOf[AnmeldeActivity]))
+  def gotoMainActivity(view: View): Unit = {
+    startActivity(new Intent(this, classOf[MainActivity]));
+    Toast.makeText(getApplicationContext(),"Speichern erfolgreich",5000).show();
+  }
 }
+
